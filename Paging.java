bcf73909482ccd046db009386ae310643acb9fe5
Melissa Lopez <mel501@nyu.edu>
2016-12-04 17:26:08 -0500

    looping

--- Paging.java
+++ Paging.java
@@ -37,9 +37,46 @@ public class Paging {
         System.out.println("The level of debugging output is " + debug + ".");
         System.out.println("********************************************\n");
 
-        int q = 3;
+        pager(machineSize, pageSize, processSize, jobMix, numRef, debug, algo);
+    }
 
-        Process[] processArr = startProcesses(machineSize, pageSize, processSize, jobMix, numRef, debug);
+    public static void pager(int machineSize, int pageSize, int processSize, int jobMix, int numRef, int debug, String algo){
+
+        // Setup stuff
+
+        Process[] processArr = startProcesses(machineSize, pageSize, processSize, jobMix, numRef, debug); printProcesses(processArr);
+        createFrameTable(machineSize, pageSize, processSize, jobMix, numRef, debug);
+
+        // Determine proper algorithm
+
+        if (algo.equals("lru")){
+            lru(machineSize, pageSize, processSize, jobMix, numRef, debug, processArr);
+        }
+    }
+
+    public static void lru(int machineSize, int pageSize, int processSize, int jobMix, int numRef, int debug, Process[] processArr){
+        System.out.printf("LRU starting! \n");
+        int numProcesses = 4;
+        if (jobMix == 1 || jobMix == 4){
+            numProcesses = 1;
+        } 
+
+        for (int k = 0; k < (int) Math.ceil((double) numRef * numProcesses / 3); k++){
+            // each process
+            for (int i = 0; i < numProcesses; i++){
+                // should go three times
+                for (int j = 0; j < 3; j++){
+                    // unless it has already gone enough times
+                    if (processArr[i].count >= numRef){
+                        break;
+                    } else {
+                        System.out.printf("Process %d \n", i+1);
+                        processArr[i].count++;
+                    }
+                    
+                }
+            }
+        }
     }
 
     /* 
@@ -47,7 +84,7 @@ public class Paging {
     */
     public static Process[] startProcesses(int machineSize, int pageSize, int processSize, int jobMix, int numRef, int debug){
         Process[] processArr;
-        if (jobMix == 1){
+        if (jobMix == 1 || jobMix == 4){
             int w = (111 * 1 + processSize) % processSize;
             processArr = new Process[1];
             processArr[0] = new Process(w);
@@ -57,7 +94,7 @@ public class Paging {
             } 
         } else {
             int k = 1;
-            processArr = new Process[1];
+            processArr = new Process[4];
             for (int i = 0; i < 4; i ++){
                 int w = (111 * 1 + processSize) % processSize;
                 processArr[i] = new Process(w);
@@ -71,15 +108,45 @@ public class Paging {
         return processArr;    
     }
 
-    // public static int[][] createFrameTable(int machineSize, int pageSize, int processSize, int jobMix, int numRef, int debug){
-    //     tableEntry[][] frameTable
+    public static Entry[][] createFrameTable(int machineSize, int pageSize, int processSize, int jobMix, int numRef, int debug){
+        Entry[][] frameTable = new Entry[machineSize/pageSize][pageSize];
+        for (int i = 0; i < machineSize/pageSize; i++){
+            for (int j = 0; j < pageSize; j++){
+                frameTable[i][j] = new Entry(-1, -1, -1, -1);
+            }
+        }
 
-    //     return frameTable;
-    // }
+        printFrameTable(frameTable);
+
+        return frameTable;
+    }
+
+    public static void printFrameTable(Entry[][] frameTable){
+        System.out.printf("+----------+----+----+----+----+----+----+----+----+----+----+\n");
+        for (int i = 0; i < frameTable.length; i++){
+            System.out.printf("| Frame %2d |", i);
+            for (int j = 0; j < frameTable[0].length; j++){
+                System.out.printf("%3d |", frameTable[i][j].address);
+            }
+            System.out.printf("\n+----------+----+----+----+----+----+----+----+----+----+----+");
+            System.out.printf("\n");
+        }
+    }
+
+    public static void printProcesses(Process[] processArr){
+        for (int i = 0; i < processArr.length; i++){
+            System.out.printf("Process %d:\n", i+1);
+            System.out.printf("     Start at: %d\n", processArr[i].w);
+            System.out.printf("     Faults: %d\n", processArr[i].faults);
+            System.out.printf("     Evictions: %d\n", processArr[i].evictions);
+            System.out.printf("     Residency Time: %d\n", processArr[i].residencyTime);
+        }
+    }
 }
 
-class Process{
+class Process {
     public int w;
+    public int count = 0;
     public int faults = 0;
     public int evictions = 0;
     public int residencyTime = 0; // time that the page was evicted minus the time it was loaded.
@@ -89,3 +156,45 @@ class Process{
     }
 
 }
+
+class Entry {
+    public int address;
+    public int loadTime;
+    public int processNum;
+    public int pageNum;
+    public int evictTime = 0;
+
+    public Entry(int address, int loadTime, int processNum, int pageNum){
+        this.address = address;
+        this.loadTime = loadTime;
+        this.processNum = processNum;
+        this.pageNum = pageNum;
+    }
+
+}
+
+// System.out.printf("LRU starting! \n");
+// int numProcesses = 4;
+// if (jobMix == 1 || jobMix == 4){
+//     numProcesses = 1;
+// } 
+
+// for (int k = 0; k < (int) Math.ceil((double) numRef * numProcesses / 3); k++){
+//     // each process
+//     for (int i = 0; i < numProcesses; i++){
+//         // should go three times
+//         for (int j = 0; j < 3; j++){
+//             // unless it has already gone enough times
+//             if (processArr[i].count >= numRef){
+//                 break;
+//             } else {
+//                 System.out.printf("Process %d \n", i+1);
+//                 processArr[i].count++;
+//             }
+            
+//         }
+//     }
+// }
+
+
+
