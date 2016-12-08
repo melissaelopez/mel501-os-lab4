83f18db57669e1c008c6f7c9d4aba7f6f00c2d16
Melissa Lopez <mel501@nyu.edu>
2016-12-08 14:29:30 -0500

    1,2,3,4,5,12

--- Paging.java
+++ Paging.java
@@ -14,33 +14,42 @@ J=4: One process with A=.75, B=.25 and C=0; one process with A=.75, B=0, and C=.
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Paging {
 
     public static void main(String[] args) {
 
-        int machineSize = Integer.parseInt(args[0]);
-        int pageSize = Integer.parseInt(args[1]);
-        int processSize = Integer.parseInt(args[2]);
-        int jobMix = Integer.parseInt(args[3]);
-        int numRef = Integer.parseInt(args[4]);
-        String algo = args[5];
-        int debug = Integer.parseInt(args[6]);
+        File inputFile = new File ("random_numbers.txt");
 
-        System.out.println("\n********************************************");
-        System.out.println("The machine size is " + machineSize + ".");  // in words
-        System.out.println("The page size is " + pageSize + ".");     // in words
-        System.out.println("The process size is " + processSize + ".");  // i.e., the references are to virtual addresses 0..S-1.
-        System.out.println("The job mix number is " + jobMix + ".");
-        System.out.println("The number of references per process is " + numRef + ".");
-        System.out.println("The replacement algorithm is " + algo + ".");
-        System.out.println("The level of debugging output is " + debug + ".");
-        System.out.println("********************************************\n");
+        try {
+            Scanner scanner = new Scanner(inputFile);
 
-        pager(machineSize, pageSize, processSize, jobMix, numRef, debug, algo);
+            int machineSize = Integer.parseInt(args[0]);
+            int pageSize = Integer.parseInt(args[1]);
+            int processSize = Integer.parseInt(args[2]);
+            int jobMix = Integer.parseInt(args[3]);
+            int numRef = Integer.parseInt(args[4]);
+            String algo = args[5];
+            int debug = Integer.parseInt(args[6]);
+
+            System.out.println("\n********************************************");
+            System.out.println("The machine size is " + machineSize + ".");  // in words
+            System.out.println("The page size is " + pageSize + ".");     // in words
+            System.out.println("The process size is " + processSize + ".");  // i.e., the references are to virtual addresses 0..S-1.
+            System.out.println("The job mix number is " + jobMix + ".");
+            System.out.println("The number of references per process is " + numRef + ".");
+            System.out.println("The replacement algorithm is " + algo + ".");
+            System.out.println("The level of debugging output is " + debug + ".");
+            System.out.println("********************************************\n");
+
+            pager(machineSize, pageSize, processSize, jobMix, numRef, debug, algo, scanner);
+        } catch (FileNotFoundException e){
+            System.out.printf("Error: File not found. \n");
+        }
     }
 
-    public static void pager(int machineSize, int pageSize, int processSize, int jobMix, int numRef, int debug, String algo){
+    public static void pager(int machineSize, int pageSize, int processSize, int jobMix, int numRef, int debug, String algo, Scanner scanner){
 
         // Setup stuff
 
@@ -49,14 +58,17 @@ public class Paging {
 
         // Determine proper algorithm
 
-        if (algo.equals("lru")){
-            lru(machineSize, pageSize, processSize, jobMix, numRef, debug, processArr, frameTable);
-        }
+
+        completeProcedure(machineSize, pageSize, processSize, jobMix, numRef, debug, processArr, frameTable, scanner, algo);
+
+        // if (algo.equals("random")){
+
+        // }   
 
         displayOutput(processArr);
     }
 
-    public static void lru(int machineSize, int pageSize, int processSize, int jobMix, int numRef, int debug, Process[] processArr, Entry[][] frameTable){
+    public static void completeProcedure(int machineSize, int pageSize, int processSize, int jobMix, int numRef, int debug, Process[] processArr, Entry[][] frameTable, Scanner scanner, String algo){
         System.out.printf("LRU starting! \n");
         int numProcesses = 4;
         if (jobMix == 1){
@@ -77,9 +89,10 @@ public class Paging {
                         // find the requested page number
 
                         int requestedPage = (processArr[i].refs[processArr[i].count] / pageSize);
-                        String message = checkFrameTable(frameTable, processArr[i], requestedPage, time, "lru", processArr);
+                        String message = checkFrameTable(frameTable, processArr[i], requestedPage, time, algo, processArr, scanner);
                         System.out.printf("Process %d references word %d (page %d) at time %d: %s \n", i+1, processArr[i].refs[processArr[i].count], requestedPage, time, message);
-                        printFrameTable(frameTable);
+                        System.out.printf("%d uses random number: %d\n", i+1, randomOS(scanner));
+                        // printFrameTable(frameTable);
                         time++;
                     }
                     
@@ -158,7 +171,7 @@ public class Paging {
         }
     }
 
-    public static String checkFrameTable(Entry[][] frameTable, Process process, int requestedPage, int time, String algo, Process[] processArr){
+    public static String checkFrameTable(Entry[][] frameTable, Process process, int requestedPage, int time, String algo, Process[] processArr, Scanner scanner){
         boolean found = false;
         int location = -1;
         for (int i = 0; i < frameTable.length; i++){
@@ -172,11 +185,11 @@ public class Paging {
             frameTable[location][0].lastTimeUsed = time;
             return "Hit in frame " + location;
         } else {
-            return "Fault, " + handleFault(frameTable, process, requestedPage, time, algo, processArr);
+            return "Fault, " + handleFault(frameTable, process, requestedPage, time, algo, processArr, scanner);
         }
     }
 
-    public static String handleFault(Entry[][] frameTable, Process process, int requestedPage, int time, String algo, Process[] processArr){
+    public static String handleFault(Entry[][] frameTable, Process process, int requestedPage, int time, String algo, Process[] processArr, Scanner scanner){
         String message = "";
         process.faults++;
 
@@ -186,21 +199,26 @@ public class Paging {
             int frame = insertPage(frameTable, process, requestedPage, highestFreeFrame, time);
             message = "using free frame " + frame;
         } else{
-            if (algo.equals("lru")){
-                // evict the one that has been unused for the longest time
+            int toEvict = 0;
+            if (algo.equals("lru")){ // evict the one that has been unused for the longest time
                 int minTime = Integer.MAX_VALUE;
-                int toEvict = -1;
+                toEvict = -1;
                 for (int i = 0; i < frameTable.length; i++){
                     if (frameTable[i][0].lastTimeUsed < minTime){
                         minTime = frameTable[i][0].lastTimeUsed;
                         toEvict = i;
                     }
                 }
-                message = "evicting page " + frameTable[toEvict][0].pageNum + " of process " + frameTable[toEvict][0].processNum + " from frame " + toEvict;
-                processArr[frameTable[toEvict][0].processNum - 1].residencyTime += (time - frameTable[toEvict][0].loadTime);
-                processArr[frameTable[toEvict][0].processNum - 1].evictions++;
-                insertPage(frameTable, process, requestedPage, toEvict, time);
             }
+            else if (algo.equals("random")){
+                int randEvict = randomOS(scanner);
+                System.out.printf("%d uses random number: %d.\n", process.processNum, randEvict);
+                toEvict = randEvict % frameTable.length;
+            }
+            message = "evicting page " + frameTable[toEvict][0].pageNum + " of process " + frameTable[toEvict][0].processNum + " from frame " + toEvict;
+            processArr[frameTable[toEvict][0].processNum - 1].residencyTime += (time - frameTable[toEvict][0].loadTime);
+            processArr[frameTable[toEvict][0].processNum - 1].evictions++;
+            insertPage(frameTable, process, requestedPage, toEvict, time);
         }
 
         // else: use the given algorithm to figure out which page to evict
@@ -229,14 +247,30 @@ public class Paging {
     }
 
     public static void displayOutput(Process[] processArr){
+        int totalFaults = 0;
+        int totalEvictions = 0;
+        float residencySum = 0;
         System.out.printf("\n");
         for (int i = 0; i < processArr.length; i++){
+            totalFaults += processArr[i].faults;
+            totalEvictions += processArr[i].evictions;
+            residencySum += (float) processArr[i].residencyTime;
             if (processArr[i].evictions == 0){
                 System.out.printf("Process %d had %d faults\n     With no evictions, the average residence is undefined. \n", i+1, processArr[i].faults);
             } else {
                 System.out.printf("Process %d had %d faults and %f average residency. \n", i+1, processArr[i].faults, (float) processArr[i].residencyTime/processArr[i].evictions);
             }
         }
+
+        if (totalEvictions == 0){
+            System.out.printf("\n     With no evictions, the overall average residency is undefined.\n");
+        } else {
+            System.out.printf("\nThe total number of faults is %d and the overall average residency is %f.\n", totalFaults, (float) residencySum/totalEvictions);
+        }
+    }
+
+    public static int randomOS(Scanner scanner){ //,int frames
+        return (scanner.nextInt()); // % frames
     }
 }
 
